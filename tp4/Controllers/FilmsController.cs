78ef46961cbe36611a805f6c170bb3d3e47c3854
masmoudi aziz <azizmasmoudi019@gmail.com>
2023-11-28 23:45:31 +0100

    first commit

---
+++ tp4/Controllers/FilmsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tp4.Services.Services;
+using tp4.Models;
+
+namespace tp4.Controllers
+{
+    public class FilmsController : Controller
+    {
+
+        private readonly FilmsService filmsService;
+
+        public FilmsController(FilmsService filmsService)
+        {
+            this.filmsService = filmsService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("Films/ordered")]
+        public IEnumerable<Films> GetFilmsOrderCroissantByName()
+        {
+            return filmsService.GetFilmsOrderCroissantByName();
+        }
+
+        [HttpGet]
+        [Route("Films/role/{id}")]
+        public IEnumerable<Films> GetFilmsByRoleId(int id)
+        {
+            return filmsService.GetFilmsByRoleId(id);
+        }
+
+        [HttpGet]
+        [Route("Films/role")]
+        public IEnumerable<Films> GetFilmsByRole(Role role)
+        {
+            return filmsService.GetFilmsByRole(role);
+        }
+
+        [HttpPost]
+        [Route("Films")]
+        public void AddFile(Films film)
+        {
+            filmsService.AddFile(film);
+        }
+        [HttpDelete]
+        [Route("Films/{id}")]
+        public void DeleteFile(int id)
+        {
+            filmsService.DeleteFile(id);
+        }
+
+
+
+        
+    }
+}
